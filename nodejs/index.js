7dbcc2d908582a922e64afde712953b08043bd20
Forcex <gurbuzeren885@gmail.com>
2021-11-01 18:23:43 +0300

    feat(nodejs): edited

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,22 +2,16 @@ const RANDOM_NUMBER = Math.floor(Math.random() * 255);
 const RANDOM_NUMBER2 = Math.floor(Math.random() * 255);
 const RANDOM_NUMBER3 = Math.floor(Math.random() * 255);
 
+function hexCompontent(x) {
+	x = x.toString(16);
+    return x.length == 1 ? "0" + x : x;
+};
 
 function rgbToHex(r,g,b) {
-	r = r.toString(16);
-	g = g.toString(16);
-	b = b.toString(16);
-	const hex = (r + g + b).toString(16);
-	if(hex.length < 6) {
-		for (let i = 0; i < 6; i++) {
-			if(hex.length == 6) {
-				break;
-			};
-			return "#" + hex + (6 - hex.length);
-		}	
-	};
-	return "#" + hex
-}
+	const hex = "" + (r + g + b).toString(16);
+	const hexCode = "#" + hexCompontent(r) + hexCompontent(g) + hexCompontent(b);
+    return hexCode;
+};
 
 
 const hexCode = rgbToHex(RANDOM_NUMBER,RANDOM_NUMBER2,RANDOM_NUMBER3);
