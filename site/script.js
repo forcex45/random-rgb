f07aaebb258b0f8084df00f404f18a5faecddb62
Forcex <gurbuzeren885@gmail.com>
2021-11-01 17:53:52 +0300

    feat(site): script edited

--- site/script.js
+++ site/script.js
@@ -1,42 +1,37 @@
 new Vue({
-	el: "#app",
-	data: {
-		hex: "",
-		rgb: ""
-	},
-	methods: {
-		randomHex() {
-		this.rgb = "";
-		
-		const r = Math.floor(Math.random() * 255).toString(16);
-		const g = Math.floor(Math.random() * 255).toString(16);
-		const b = Math.floor(Math.random() * 255).toString(16);
+    el: "#app",
+    data: {
+        hex: "",
+        rgb: ""
+    },
+    methods: {
+        rgbToHex(r, g, b) {
+            const hex = (r + g + b);
 
-		const hex = (r + g + b);
-		if(hex.length < 6) {
-			for (let i = 0; i < 6; i++) {
-				if(hex.length == 6) {
-					break;
-				};
-			const hexCode = "#" + hex + (6 - hex.length);
-			document.body.style.background = hexCode;
-			this.hex = hexCode
-			}	
-		};
-			const hexCode = "#" + hex;
-			document.body.style.background = hexCode;
-			this.hex = hexCode;
-		},
-		randomRGB() {
-		this.hex = "";
-		const RANDOM_NUMBER = Math.floor(Math.random() * 255);
-		const RANDOM_NUMBER2 = Math.floor(Math.random() * 255);
-		const RANDOM_NUMBER3 = Math.floor(Math.random() * 255);
+            if (hex.length < 6) {
+                for (let i = 0; i < 6; i++) {
+                    if (hex.length == 6) {
+                        break;
+                    };
+                    const hexCode = "#" + hex + (6 - hex.length);
+                    this.hex = hexCode
+                    return hexCode;
+                }
+            };
+            const hexCode = "#" + hex;
+            this.hex = hexCode;
+            return hexCode;
+        },
+        randomRGB() {
+            const RANDOM_NUMBER = Math.floor(Math.random() * 255);
+            const RANDOM_NUMBER2 = Math.floor(Math.random() * 255);
+            const RANDOM_NUMBER3 = Math.floor(Math.random() * 255);
 
-		const rgbCode = `rgb(${RANDOM_NUMBER},${RANDOM_NUMBER2},${RANDOM_NUMBER3})`;
-		document.body.style.background = rgbCode;
+            const rgbCode = `rgb(${RANDOM_NUMBER},${RANDOM_NUMBER2},${RANDOM_NUMBER3})`;
+            this.rgbToHex(RANDOM_NUMBER, RANDOM_NUMBER2, RANDOM_NUMBER3)
 
-		this.rgb = rgbCode;
-		}
-	}
+            document.body.style.background = rgbCode;
+            this.rgb = rgbCode;
+        }
+    }
 })
